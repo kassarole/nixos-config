f3a8966fdd7aa907463a6554b7ceba36259bf776
kass <kass@basedzone.xyz>
2025-06-30 16:27:14 -0500

    typos

--- home/kass-fw16.nix
+++ home/kass-fw16.nix
@@ -5,7 +5,7 @@
   home.homeDirectory = "/home/krode";
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
-  programs.zsh.enable = true;
+
   home.packages = with pkgs; [
     vesktop
     vscode
@@ -23,25 +23,24 @@
     userEmail = "kass@basedzone.xyz";
   };
   programs.zsh = {
-    enable=true;
-    enableCompletions = true;
-    autosuggestions.enable = true;
+    enable = true;
+    enableAutosuggestions = true;
     syntaxHighlighting.enable = true;
     shellAliases = {
       update = "sudo nixos-rebuild switch --flake /home/krode/nixos-config#kass-fw16";
       build = "sudo nixos-rebuild build --flake /home/krode/nixos-config#kass-fw16";
-    }
+    };
     oh-my-zsh = {
       enable = true;
       plugins = [ "git"];
-      theme = "half-life"
+      theme = "half-life";
     };
   };
   programs.ghostty = {
-    enable=true;
+    enable = true;
     enableZshIntegration = true;
     settings = {
       theme = "catppuccin-mocha";
-    }
-  }
+    };
+  };
 }
