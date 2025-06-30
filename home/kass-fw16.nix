fb368d6ce4bfdfeaa50120df98db9b43e1b2840f
kass <kass@basedzone.xyz>
2025-06-30 15:48:14 -0500

    Extensions are kinda busted

--- home/kass-fw16.nix
+++ home/kass-fw16.nix
@@ -22,13 +22,4 @@
     userName = "kass";
     userEmail = "kass@basedzone.xyz";
   };
-  programs.vscode = {
-    enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      pinage404.nix-extension-pack
-      ms-python.python
-      unthrottled.doki-theme
-      ms-vscode-remote.vscode-remote-extensionpack
-    ]
-  };
 }
