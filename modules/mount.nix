0ee0deb069a9171c4e6af35737859a183380eae1
kass <kass@basedzone.xyz>
2025-06-30 21:08:42 -0500

    Fix perms

--- modules/mount.nix
+++ modules/mount.nix
@@ -6,7 +6,8 @@
     fsType = "cifs";
     options = let 
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-    in ["${automount_opts},credentials=/etc/nixos/credentials/USB2"];
+    in [
+      "${automount_opts},credentials=/etc/nixos/credentials/USB2,rw,file_mode=0777,dir_mode=0777"
+    ];
   };
 }
