cd4458f63da0a558731f028a311dcad0ae47c185
= <=>
2025-07-01 11:02:18 -0500

    Adding new host

--- flake.nix
+++ flake.nix
@@ -9,16 +9,31 @@
   };
   outputs = { self, nixpkgs, flake-utils, home-manager, ... }:
     let
+      # Define per-host module lists here
+      hostModules = {
+        kass-fw16 = [
+          ./modules/gaming.nix
+          ./modules/mount.nix
+        ];
+        kass-dev-nix = [
+          ./modules/mount.nix
+        ];
+        # Add more hosts and their modules as needed
+        # other-host = [ ./modules/other.nix ];
+      };
+
       mkHost = name: system: {
         ${name} = nixpkgs.lib.nixosSystem {
           inherit system;
-          modules = [
-            ./hosts/${name}/configuration.nix
-            ./hosts/${name}/hardware-configuration.nix
-            ./modules/common.nix
-            ./modules/gaming.nix
-            ./modules/mount.nix
-            home-manager.nixosModules.home-manager
+          modules =
+            [
+              ./hosts/${name}/configuration.nix
+              ./hosts/${name}/hardware-configuration.nix
+              ./modules/common.nix
+            ]
+            ++ (hostModules.${name} or [])
+            ++ [
+              home-manager.nixosModules.home-manager
               {
                 home-manager.useGlobalPkgs = true;
                 home-manager.useUserPackages = true;
@@ -26,10 +41,12 @@
                 home-manager.users.krode = import ./home/${name}.nix;
               }
             ];
-          };
         };
-      in {
-        nixosConfigurations = mkHost "kass-fw16" "x86_64-linux";
       };
+    in {
+      nixosConfigurations = mkHost "kass-fw16" "x86_64-linux" // mkHost "kass-dev-nix" "x86_64-linux";
+      # Add more hosts here as needed
+      # // mkHost "other-host" "x86_64-linux"
+    };
 }
 
---
+++ home/kass-dev-nix.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "krode";
+  home.homeDirectory = "/home/krode";
+  home.stateVersion = "25.05";
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    vscode
+    hyfetch
+
+  ];
+  programs.git = {
+    enable=true;
+    userName = "kass";
+    userEmail = "kass@basedzone.xyz";
+  };
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      update = "sudo nixos-rebuild switch --flake /home/krode/nixos-config#kass-dev-nix";
+      build = "sudo nixos-rebuild build --flake /home/krode/nixos-config#kass-dev-nix";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git"];
+      theme = "half-life";
+    };
+  };
+}
