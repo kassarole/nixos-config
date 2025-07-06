9b69750567f0bc5748ec3bd7d4334f0b54f052a9
kass <kass@basedzone.xyz>
2025-07-05 19:38:03 -0500

    Rosetta update

--- modules/darwin-common.nix
+++ modules/darwin-common.nix
@@ -10,6 +10,15 @@
     ncdu
     nettools
   ];
+  system.defaults.dock.show-recents = false;
+  system.activationScripts.installRosetta.text = ''
+    if [[ $(uname -m) == "arm64" ]]; then
+      if ! /usr/bin/pgrep oahd &>/dev/null; then
+        echo "Installing Rosetta 2..."
+        /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+      fi
+    fi
+  '';
   services.openssh.enable = true;
   programs.zsh.enable = true;
   system.primaryUser = "krode";
