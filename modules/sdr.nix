dd5ba8b24a371ca825bc37ef9c84ae5b634be974
kass <kass@basedzone.xyz>
2025-07-18 12:22:08 -0500

    Add rtl_tcp service

--- modules/sdr.nix
+++ modules/sdr.nix
@@ -2,4 +2,16 @@
 {
   hardware.rtl-sdr.enable = true;
   users.users.krode.extraGroups = [ "plugdev" ];
+
+  systemd.services.rtl_tcp = {
+    description = "RTL-SDR TCP server";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.rtl-sdr}/bin/rtl_tcp -a 0.0.0.0 -p 1234";
+      Restart = "on-failure";
+      User = "krode";
+      Group = "plugdev";
+    };
+  };
 }
