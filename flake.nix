002936a6a657ca5aed5ad6a2b1fb2fba9081d31c
kass <kass@basedzone.xyz>
2025-07-18 11:56:09 -0500

    added templating for hermes

--- flake.nix
+++ flake.nix
@@ -46,6 +46,8 @@
           ./modules/virtualmachine.nix
           ./modules/dev.nix
         ];
+        hermes = [
+        ];
         # Add more hosts and their modules as needed
         # other-host = [ ./modules/other.nix ];
       };
@@ -95,7 +97,7 @@
         };
       };
     in {
-      nixosConfigurations = mkHost "metis" "x86_64-linux" // mkHost "selene" "x86_64-linux" // mkHost "hestia" "x86_64-linux" // mkHost "hephaestus" "x86_64-linux" // mkHost "eros" "x86_64-linux" // mkHost "gaia" "x86_64-linux";
+      nixosConfigurations = mkHost "metis" "x86_64-linux" // mkHost "selene" "x86_64-linux" // mkHost "hestia" "x86_64-linux" // mkHost "hephaestus" "x86_64-linux" // mkHost "eros" "x86_64-linux" // mkHost "gaia" "x86_64-linux" // mkHost "hermes" "x86_64-linux";
       darwinConfigurations = mkDarwinHost "nike" "aarch64-darwin";
       # Add more hosts here as needed
       # // mkHost "other-host" "x86_64-linux"
---
+++ home/hermes.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, lib, ... }:
+
+{
+  home.username = "krode";
+  home.homeDirectory = "/home/krode";
+  home.stateVersion = "25.05";
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+  ];
+  programs.git = {
+    enable=true;
+    userName = "kass";
+    userEmail = "kass@basedzone.xyz";
+  };
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      update = "sudo nixos-rebuild switch --flake /home/krode/nixos-config#hermes";
+      build = "sudo nixos-rebuild build --flake /home/krode/nixos-config#hermes";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git"];
+      theme = "dpoggi";
+    };
+  };
+  home.file.".zshrc".text = lib.mkAfter ''
+    export TERM=xterm-256color
+  '';
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+}
